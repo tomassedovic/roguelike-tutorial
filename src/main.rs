890002c9d157fc86a72d8b00b875469bd71cc1e7
Tomas Sedovic <tomas@sedovic.cz>
2015-09-29 22:08:08 +0200

    Use `join` instead of `connect`

--- src/main.rs
+++ src/main.rs
@@ -873,7 +873,7 @@ fn get_names_under_mouse(mouse: Mouse, objects: &[Object], fov_map: &FovMap) -> 
     objects.iter().filter(
         |obj| {
             obj.pos() == (x, y) && fov_map.is_in_fov(obj.x, obj.y)
-        }).map(|obj| obj.name.clone()).collect::<Vec<_>>().connect(", ")
+        }).map(|obj| obj.name.clone()).collect::<Vec<_>>().join(", ")
 }
 
 fn render_all(objects: &[Object], game: &mut Game, tcod: &mut TcodState) {
