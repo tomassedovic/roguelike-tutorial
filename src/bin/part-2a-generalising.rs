1dcbe6886204256ecc2c02683479ca4a5337cea3
Tomas Sedovic <tomas@sedovic.cz>
2015-12-04 13:50:19 +0100

    Add the offscreen and generalising sections

--- src/bin/part-2a-generalising.rs
+++ src/bin/part-2a-generalising.rs
@@ -1,7 +1,7 @@
 extern crate tcod;
 
 use tcod::console::*;
-use tcod::colors;
+use tcod::colors::{self, Color};
 
 // actual size of the window
 const SCREEN_WIDTH: i32 = 80;
@@ -10,7 +10,44 @@ const SCREEN_HEIGHT: i32 = 50;
 const LIMIT_FPS: i32 = 20;  // 20 frames-per-second maximum
 
 
-fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+/// This is a generic object: the player, a monster, an item, the stairs...
+/// It's always represented by a character on screen.
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object {
+            x: x,
+            y: y,
+            char: char,
+            color: color,
+        }
+    }
+
+    /// move by the given amount
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    /// set the color and then draw the character that represents this object at its position
+    pub fn draw(&self, con: &mut Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+
+    /// Erase the character that represents this object
+    pub fn clear(&self, con: &mut Console) {
+        con.put_char(self.x, self.y, ' ', BackgroundFlag::None);
+    }
+}
+
+fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
@@ -24,10 +61,10 @@ fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool 
         Key { code: Escape, .. } => return true,  // exit game
 
         // movement keys
-        Key { code: Up, .. } => *player_y -= 1,
-        Key { code: Down, .. } => *player_y += 1,
-        Key { code: Left, .. } => *player_x -= 1,
-        Key { code: Right, .. } => *player_x += 1,
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
 
         _ => {},
     }
@@ -45,20 +82,33 @@ fn main() {
     tcod::system::set_fps(LIMIT_FPS);
     let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    let mut player_x = SCREEN_WIDTH / 2;
-    let mut player_y = SCREEN_HEIGHT / 2;
+    // create object representing the player
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+
+    // create an NPC
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
+
+    // the list of objects with those two
+    let mut objects = [player, npc];
 
     while !root.window_closed() {
-        con.set_default_foreground(colors::WHITE);
-        con.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        // draw all objects in the list
+        for object in &objects {
+            object.draw(&mut con);
+        }
 
+        // blit the contents of "con" to the root console and present it
         blit(&mut con, (0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), &mut root, (0, 0), 1.0, 1.0);
         root.flush();
 
-        con.put_char(player_x, player_y, ' ', BackgroundFlag::None);
+        // erase all objects at their old locations, before they move
+        for object in &objects {
+            object.clear(&mut con)
+        }
 
         // handle keys and exit game if needed
-        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut root, player);
         if exit {
             break
         }
