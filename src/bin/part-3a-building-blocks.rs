4665d1191961984e294aec2f0a91e98232078aca
Tomas Sedovic <tomas@sedovic.cz>
2015-12-05 16:29:19 +0100

    Add stubs for the third part

---
+++ src/bin/part-3a-building-blocks.rs
@@ -0,0 +1,176 @@
+extern crate tcod;
+
+use tcod::console::*;
+use tcod::colors::{self, Color};
+
+// actual size of the window
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+// size of the map
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+
+const LIMIT_FPS: i32 = 20;  // 20 frames-per-second maximum
+
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+
+type Map = Vec<Vec<Tile>>;
+
+/// A tile of the map and its properties
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile{blocked: false, block_sight: false}
+    }
+
+    pub fn wall() -> Self {
+        Tile{blocked: true, block_sight: true}
+    }
+}
+
+/// This is a generic object: the player, a monster, an item, the stairs...
+/// It's always represented by a character on screen.
+#[derive(Debug)]
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object {
+            x: x,
+            y: y,
+            char: char,
+            color: color,
+        }
+    }
+
+    /// move by the given amount, if the destination is not blocked
+    pub fn move_by(&mut self, dx: i32, dy: i32, map: &Map) {
+        if !map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
+    }
+
+    /// set the color and then draw the character that represents this object at its position
+    pub fn draw(&self, con: &mut Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+
+    /// Erase the character that represents this object
+    pub fn clear(&self, con: &mut Console) {
+        con.put_char(self.x, self.y, ' ', BackgroundFlag::None);
+    }
+}
+
+fn make_map() -> Map {
+    // fill map with "unblocked" tiles
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    // place two pillars to test the map
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+
+    map
+}
+
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+    // go through all tiles, and set their background color
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = map[x as usize][y as usize].block_sight;
+            if wall {
+                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+
+    // draw all objects in the list
+    for object in objects {
+        object.draw(con);
+    }
+
+    // blit the contents of "con" to the root console
+    blit(con, (0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), root, (0, 0), 1.0, 1.0);
+}
+
+fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = root.wait_for_keypress(true);
+    match key {
+        Key { code: Enter, alt: true, .. } => {
+            // Alt+Enter: toggle fullscreen
+            let fullscreen = root.is_fullscreen();
+            root.set_fullscreen(!fullscreen);
+        }
+        Key { code: Escape, .. } => return true,  // exit game
+
+        // movement keys
+        Key { code: Up, .. } => player.move_by(0, -1, map),
+        Key { code: Down, .. } => player.move_by(0, 1, map),
+        Key { code: Left, .. } => player.move_by(-1, 0, map),
+        Key { code: Right, .. } => player.move_by(1, 0, map),
+
+        _ => {},
+    }
+
+    false
+}
+
+fn main() {
+    let mut root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust/libtcod tutorial")
+        .init();
+    tcod::system::set_fps(LIMIT_FPS);
+    let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    // create object representing the player
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+
+    // create an NPC
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
+
+    // the list of objects with those two
+    let mut objects = [player, npc];
+
+    // generate map (at this point it's not drawn to the screen)
+    let map = make_map();
+
+    while !root.window_closed() {
+        // render the screen
+        render_all(&mut root, &mut con, &objects, &map);
+
+        root.flush();
+
+        // erase all objects at their old locations, before they move
+        for object in &objects {
+            object.clear(&mut con)
+        }
+
+        // handle keys and exit game if needed
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut root, player, &map);
+        if exit {
+            break
+        }
+    }
+}
