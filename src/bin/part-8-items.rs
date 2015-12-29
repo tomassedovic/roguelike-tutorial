02077f3e1d386ba15c19bc3aa4383e31dcad3a3a
Tomas Sedovic <tomas@sedovic.cz>
2015-12-29 21:16:28 +0100

    Finish the Inventory Menu section of part 8

--- src/bin/part-8-items.rs
+++ src/bin/part-8-items.rs
@@ -3,6 +3,7 @@ extern crate rand;
 
 use std::cmp;
 
+use std::ascii::AsciiExt;
 use tcod::console::*;
 use tcod::colors::{self, Color};
 use tcod::input::{self, Event, Key, Mouse};
@@ -24,6 +25,7 @@ const PANEL_Y: i32 = SCREEN_HEIGHT - PANEL_HEIGHT;
 const MSG_X: i32 = BAR_WIDTH + 2;
 const MSG_WIDTH: i32 = SCREEN_WIDTH - BAR_WIDTH - 2;
 const MSG_HEIGHT: usize = PANEL_HEIGHT as usize - 1;
+const INVENTORY_WIDTH: i32 = 50;
 
 //parameters for dungeon generator
 const ROOM_MAX_SIZE: i32 = 10;
@@ -590,6 +592,69 @@ fn player_move_or_attack(dx: i32, dy: i32, map: &Map, objects: &mut [Object],
     }
 }
 
+fn menu<T: AsRef<str>>(header: &str, options: &[T], width: i32,
+                       root: &mut Root) -> Option<usize> {
+    assert!(options.len() <= 26, "Cannot have a menu with more than 26 options.");
+
+    // calculate total height for the header (after auto-wrap) and one line per option
+    let header_height = root.get_height_rect(0, 0, width, SCREEN_HEIGHT, header);
+    let height = options.len() as i32 + header_height;
+
+    // create an off-screen console that represents the menu's window
+    let mut window = Offscreen::new(width, height);
+
+    // print the header, with auto-wrap
+    window.set_default_foreground(colors::WHITE);
+    window.print_rect_ex(0, 0, width, height, BackgroundFlag::None, TextAlignment::Left, header);
+
+    // print all the options
+    for (index, option_text) in options.iter().enumerate() {
+        let menu_letter = ('a' as usize + index) as u8 as char;
+        let text = format!("({}) {}", menu_letter, option_text.as_ref());
+        window.print_ex(0, header_height + index as i32,
+                        BackgroundFlag::None, TextAlignment::Left, text);
+    }
+
+    // blit the contents of "window" to the root console
+    let x = SCREEN_WIDTH / 2 - width / 2;
+    let y = SCREEN_HEIGHT / 2 - height / 2;
+    tcod::console::blit(&mut window, (0, 0), (width, height), root, (x, y), 1.0, 0.7);
+
+    // present the root console to the player and wait for a key-press
+    root.flush();
+    let key = root.wait_for_keypress(true);
+
+    // convert the ASCII code to an index; if it corresponds to an option, return it
+    if key.printable.is_alphabetic() {
+        let index = key.printable.to_ascii_lowercase() as usize - 'a' as usize;
+        if index < options.len() {
+            Some(index)
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
+fn inventory_menu(inventory: &[Object], header: &str, root: &mut Root) -> Option<usize> {
+    // how a menu with each item of the inventory as an option
+    let options = if inventory.len() == 0 {
+        vec!["Inventory is empty.".into()]
+    } else {
+        inventory.iter().map(|item| { item.name.clone() }).collect()
+    };
+
+    let inventory_index = menu(header, &options, INVENTORY_WIDTH, root);
+
+    // if an item was chosen, return it
+    if inventory.len() > 0 {
+        inventory_index
+    } else {
+        None
+    }
+}
+
 fn handle_keys(key: Key, root: &mut Root, map: &Map, objects: &mut Vec<Object>,
                inventory: &mut Vec<Object>, messages: &mut Messages) -> PlayerAction {
     use tcod::input::KeyCode::*;
@@ -631,6 +696,14 @@ fn handle_keys(key: Key, root: &mut Root, map: &Map, objects: &mut Vec<Object>,
             if let Some(item_id) = item_id {
                 pick_item_up(item_id, objects, inventory, messages);
             }
+            TookTurn
+        }
+        (Key { printable: 'i', .. }, true) => {
+            // show the inventory
+            inventory_menu(
+                inventory,
+                "Press the key next to an item to use it, or any other to cancel.\n",
+                root);
             TookTurn
         }
 
