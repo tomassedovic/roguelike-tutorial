97fbc874412fb034de38fcf194af0b3f9d74a36d
Tomas Sedovic <tomas@sedovic.cz>
2016-04-03 22:44:31 +0200

    Finish the dungeon level bit.

--- src/bin/part-11-dungeon-progression.rs
+++ src/bin/part-11-dungeon-progression.rs
@@ -123,6 +123,7 @@ struct Object {
     fighter: Option<Fighter>,
     ai: Option<Ai>,
     item: Option<Item>,
+    always_visible: bool,
 }
 
 impl Object {
@@ -138,6 +139,7 @@ impl Object {
             fighter: None,
             ai: None,
             item: None,
+            always_visible: false,
         }
     }
 
@@ -650,7 +652,8 @@ fn make_map(objects: &mut Vec<Object>) -> Map {
 
     // create stairs at the center of the last room
     let (last_room_x, last_room_y) = rooms[rooms.len() - 1].center();
-    let stairs = Object::new(last_room_x, last_room_y, '<', "stairs", colors::WHITE, false);
+    let mut stairs = Object::new(last_room_x, last_room_y, '<', "stairs", colors::WHITE, false);
+    stairs.always_visible = true;
     objects.push(stairs);
 
     map
@@ -698,7 +701,7 @@ fn place_objects(room: Rect, map: &Map, objects: &mut Vec<Object>) {
         // only place it if the tile is not blocked
         if !is_blocked(x, y, map, objects) {
             let dice = rand::random::<f32>();
-            let item = if dice < 0.7 {
+            let mut item = if dice < 0.7 {
                 // create a healing potion (70% chance)
                 let mut object = Object::new(x, y, '!', "healing potion", colors::VIOLET, false);
                 object.item = Some(Item::Heal);
@@ -721,6 +724,7 @@ fn place_objects(room: Rect, map: &Map, objects: &mut Vec<Object>) {
                 object.item = Some(Item::Confuse);
                 object
             };
+            item.always_visible = true;
             objects.push(item);
         }
     }
@@ -736,6 +740,7 @@ fn next_level(tcod: &mut Tcod, objects: &mut Vec<Object>, game: &mut Game) {
 
     game.log.add("After a rare moment of peace, you descend deeper into \
                   the heart of the dungeon...", colors::RED);
+    game.dungeon_level += 1;
     game.map = make_map(objects);
     initialise_fov(&game.map, tcod);
 }
@@ -816,7 +821,13 @@ fn render_all(tcod: &mut Tcod, objects: &[Object], game: &mut Game, fov_recomput
         }
     }
 
-    let mut to_draw: Vec<_> = objects.iter().filter(|o| tcod.fov.is_in_fov(o.x, o.y)).collect();
+    let mut to_draw: Vec<_> = objects
+        .iter()
+        .filter(|o| {
+            tcod.fov.is_in_fov(o.x, o.y) ||
+                (o.always_visible && game.map[o.x as usize][o.y as usize].explored)
+        })
+        .collect();
     // sort so that non-blocknig objects come first
     to_draw.sort_by(|o1, o2| { o1.blocks.cmp(&o2.blocks) });
     // draw the objects in the list
@@ -848,6 +859,9 @@ fn render_all(tcod: &mut Tcod, objects: &[Object], game: &mut Game, fov_recomput
     let hp = objects[PLAYER].fighter.map_or(0, |f| f.hp);
     let max_hp = objects[PLAYER].fighter.map_or(0, |f| f.max_hp);
     render_bar(&mut tcod.panel, 1, 1, BAR_WIDTH, "HP", hp, max_hp, colors::LIGHT_RED, colors::DARKER_RED);
+
+    tcod.panel.print_ex(1, 3, BackgroundFlag::None, TextAlignment::Left,
+                        format!("Dungeon level: {}", game.dungeon_level));
 
     // display names of objects under the mouse
     tcod.panel.set_default_foreground(colors::LIGHT_GREY);
@@ -1083,6 +1097,7 @@ struct Game {
     map: Map,
     log: Messages,
     inventory: Vec<Object>,
+    dungeon_level: u32,
 }
 
 trait MessageLog {
@@ -1111,6 +1126,7 @@ fn new_game(tcod: &mut Tcod) -> (Vec<Object>, Game) {
         // create the list of game messages and their colors, starts empty
         log: vec![],
         inventory: vec![],
+        dungeon_level: 1,
     };
 
     initialise_fov(&game.map, tcod);
