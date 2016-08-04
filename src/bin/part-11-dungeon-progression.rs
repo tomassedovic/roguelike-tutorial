8b4b1446fe4a954f04bb9a8a869f2dfe875b8cff
Tomas Sedovic <tomas@sedovic.cz>
2016-08-05 00:11:17 +0200

    Fix broken healing potions
    We used `let Some(mut fighter) = fighter` instead of `let Some(ref mut
    fighter) = fighter`.
    
    Since the Fighter struct is a copy, this caused healing via potions to
    not work.

--- src/bin/part-11-dungeon-progression.rs
+++ src/bin/part-11-dungeon-progression.rs
@@ -221,7 +221,7 @@ impl Object {
 
     /// heal by the given amount, without going over the maximum
     pub fn heal(&mut self, amount: i32) {
-        if let Some(mut fighter) = self.fighter {
+        if let Some(ref mut fighter) = self.fighter {
             fighter.hp += amount;
             if fighter.hp > fighter.max_hp {
                 fighter.hp = fighter.max_hp;
